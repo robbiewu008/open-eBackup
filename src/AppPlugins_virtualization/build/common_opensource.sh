69e17e9b4e304a6f20f109bd586f54ee5ba463af
* <*>
2024-09-27 23:51:02 +0800

    Match-id-5f30c74212176af3e8efb60297baf4abad29ae65

--- src/AppPlugins_virtualization/build/common_opensource.sh
+++ src/AppPlugins_virtualization/build/common_opensource.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/# 
+#
 #  This file is a part of the open-eBackup project.
 #  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 #  If a copy of the MPL was not distributed with this file, You can obtain one at
