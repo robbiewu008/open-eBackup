fa791379b3d5834ae50859422658e3ca32617110
* <*>
2024-09-27 17:40:46 +0800

    Match-id-2e478a2fe390c4142bac3619d00878e4a8c1428d

--- build/package_open_source_final.sh
+++ build/package_open_source_final.sh
@@ -1,10 +1,25 @@
 #!/usr/bin
+########################################
+#  This file is part of the open-eBackup project.
+# Copyright (c) 2024 Huawei Technologies Co.,Ltd.
+#
+# open-eBackup is licensed under MPL v2.
+# You can use this software according to the terms and conditions of the MPL v2.
+# You may obtain a copy of MPL v2 at:
+#
+#          https://www.mozilla.org/en-US/MPL/2.0
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the MPL v2 for more details.
+########################################
 set -x
 
 # env params
-export WORKSPACE=${1:-"/usr1/"} # 代码存放环境
-export code_path=${2:-"${WORKSPACE}/REST_API"}
-export binary_path=${3:-"${WORKSPACE}/open-source-obligation"}
+export WORKSPACE=${1:-"/usr1/"} # 工作目录
+export code_path=${2:-"${WORKSPACE}/REST_API"}  # 开源代码存放路径
+export binary_path=${3:-"${WORKSPACE}/open-source-obligation"} # 开源二进制文件存放路径
 export tag_image="release"
 export BUILD_TYPE="release"
 export MS_IMAGE_TAG=${5:"1.6.RC2.010"}
@@ -18,14 +33,10 @@ sh ProtectAgent/component/protectagent/protectagent/Agent/ci/script/build_pkg_op
 # 镜像模块编译
 
 cd ${WORKSPACE}/REST_API/src
-sh Infrastructure_OM/infrastructure/script/build_opensource.sh dorado mspkg
+sh Infrastructure_OM/infrastructure/script/build_opensource.sh mspkg
 
 cd ${WORKSPACE}/REST_API/src
-cd DataEnableEngine/build
-sh build_opensource.sh all "${binary_path}"
-
-cd ${WORKSPACE}/REST_API/src
-sh ProtectManager/CI/script/build_opensource.sh "dorado" "ProtectManager" ${binary_path}/ "$@"
+sh ProtectManager/CI/script/build_opensource.sh ${binary_path}/ "$@"
 
 cd ${WORKSPACE}/REST_API/src
 sh DataMoveEngine/build/build_opensource.sh ${binary_path}/
@@ -33,13 +44,6 @@ sh DataMoveEngine/build/build_opensource.sh ${binary_path}/
 
 # build final pkg
 cd ${WORKSPACE}/REST_API/src
-# DEE 大包
-# open-eBackup_1.x.0_DataManager.tgz
-export BUILD_MODULE=system_dee
-export BUILD_PKG_TYPE=OpenSource
-
-# 镜像制作
-sh DPAProduct/CI/script/Package_100P.sh
 
 # DME 大包
 # open-eBackup_1.x.0_MediaServer.tgz
