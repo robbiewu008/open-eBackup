f303fc2a0004658d7aade941bf8d95d9ade556fc
* <*>
2024-09-27 21:39:37 +0800

    Match-id-315ee95ddcd9955bdb80c78a29d8e259883d1345

--- src/ProtectManager/component/PM_GUI/src/service/console/src/app/business/system/network-config/ip-config-list/ip-config-list.component.ts
+++ src/ProtectManager/component/PM_GUI/src/service/console/src/app/business/system/network-config/ip-config-list/ip-config-list.component.ts
@@ -1,3 +1,15 @@
+/*
+ * This file is a part of the open-eBackup project.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) [2024] Huawei Technologies Co.,Ltd.
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ */
 import { Component, OnInit, Injectable } from '@angular/core';
 import { MODAL_COMMON, I18NService } from 'app/shared';
 import { DrawModalService } from 'app/shared/services/draw-modal.service';
--- src/ProtectManager/component/PM_GUI/src/service/src/main/java/com/huawei/emeistor/console/util/SHA256Encryptor.java
+++ src/ProtectManager/component/PM_GUI/src/service/src/main/java/com/huawei/emeistor/console/util/SHA256Encryptor.java
@@ -1,7 +1,15 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2019-2020. All rights reserved.
- */
-
+* This file is a part of the open-eBackup project.
+* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+* If a copy of the MPL was not distributed with this file, You can obtain one at
+* http://mozilla.org/MPL/2.0/.
+*
+* Copyright (c) [2024] Huawei Technologies Co.,Ltd.
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*/
 package com.huawei.emeistor.console.util;
 
 import com.huawei.emeistor.console.exterattack.ExterAttack;
