af0c2283cab5ec56d3e9aab0c889744c8dae8f5f
* <*>
2024-10-11 10:07:44 +0800

    Match-id-bc78607f2300aadb5133229b6cd258a794eb5f26

--- src/Infrastructure_OM/om/build_opensource.sh
+++ src/Infrastructure_OM/om/build_opensource.sh
@@ -24,16 +24,12 @@ LCRP_XML_PATH=${PM_MS_DIR}/conf
 CI_DIR=${PM_MS_DIR}/../ci
 sh ${CI_DIR}/script/open_comm_param.sh
 
-code_branch=$(echo ${CODE_BRANCH} | tr [A-Z] [a-z])
-echo tag_image=${tag_image}
-echo code_branch=${code_branch}
-
 function build_image() {
 
     NAME=om
-    echo "Begin to build $MS_NAME"
     L_TAG=${NAME}':'${MS_IMAGE_TAG}
     MS_NAME=$(echo ${L_TAG} | sed 's/:/-/g')
+    echo "Begin to build $MS_NAME"
 
     mkdir -p "${PM_MS_DIR}/tmp/$MS_NAME/"
 
