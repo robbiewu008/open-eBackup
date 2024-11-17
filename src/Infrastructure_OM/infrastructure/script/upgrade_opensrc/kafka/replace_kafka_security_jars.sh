1a0d1a6da40330452efad1f8159f04b3737f505b
* <*>
2024-11-17 16:23:59 +0800

    Match-id-7952505a93d13130fc7cbfaa0854f418aa67b0ac

--- src/Infrastructure_OM/infrastructure/script/upgrade_opensrc/kafka/replace_kafka_security_jars.sh
+++ src/Infrastructure_OM/infrastructure/script/upgrade_opensrc/kafka/replace_kafka_security_jars.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#########################################
-# Copyright (c) 2012-2015 Huawei .
-# All rights reserved.
+########################################
+#  This file is part of the open-eBackup project.
+# Copyright (c) 2024 Huawei Technologies Co.,Ltd.
 #
-# Please send feedback to http://www.huawei.com
+# open-eBackup is licensed under MPL v2.
+# You can use this software according to the terms and conditions of the MPL v2.
+# You may obtain a copy of MPL v2 at:
 #
-# Function 替换开源组件kafka被动依赖jar包
-# 
-# 安全jar包 来源公司通用仓（同步到CMC仓）
-# 
+#          https://www.mozilla.org/en-US/MPL/2.0
+#
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the MPL v2 for more details.
 ########################################
 
 CURRENT_PATH=$(cd `dirname $0`; pwd)
