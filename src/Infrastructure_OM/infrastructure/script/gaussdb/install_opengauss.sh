b5a7c24f44d4f419a8ac9d02ce70fa2eb5c248d0
* <*>
2025-03-18 09:47:19 +0800

    Match-id-c23e5c17c54d31f740a032de3b343fe719aec386

--- src/Infrastructure_OM/infrastructure/script/gaussdb/install_opengauss.sh
+++ src/Infrastructure_OM/infrastructure/script/gaussdb/install_opengauss.sh
@@ -170,6 +170,35 @@ fi
 chmod 700 data
 check_result "$?" "${LINENO} chmod 700 data"
 
+CLUSTER_ROLE=""
+if [[ ${DEPLOY_TYPE} == "d9" ]]; then
+    config_maps=$(LD_PRELOAD=/usr/lib64/libSecurityStarter.so curl --cacert ${rootCAFile} -X GET -H "Authorization: Bearer $tokenFile" https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/dpa/configmaps/cluster-conf follow@-H)
+    is_exist=$(echo "${config_maps}" | python3 -c "import sys, json;print(json.load(sys.stdin).get('data',{}).get('CLUSTER_ROLE'))")
+    CLUSTER_ROLE=${is_exist}
+    if [ "${is_exist}" == "None" ];then
+        log_info "Start to add cluster conf"
+        PAYLOAD="{\"data\":{\"CLUSTER_ROLE\":\"\",\"CLUSTER_ESN\":\"\",\"CLUSTER_SERVICE_IPS\":\"\",\"CLUSTER_ADDING_FLAG\":\"\"}}"
+        for i in {1..3}
+        do
+            curl --cacert ${rootCAFile} \
+              -X PATCH \
+              -H "Content-Type: application/strategic-merge-patch+json" \
+              -H "Authorization: Bearer ${tokenFile}" \
+              --data "${PAYLOAD}" \
+              https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/dpa/configmaps/cluster-conf
+            log_info "${LINENO} Start to check cluster conf"
+            config_maps=$(LD_PRELOAD=/usr/lib64/libSecurityStarter.so curl --cacert ${rootCAFile} -X GET -H "Authorization: Bearer $tokenFile" https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/dpa/configmaps/cluster-conf follow@-H)
+            is_exist=$(echo "${config_maps} " | python3 -c "import sys, json;print(json.load(sys.stdin).get('data',{}).get('CLUSTER_ROLE'))")
+            if [ "${is_exist}" != "None" ];then
+                log_info "${LINENO} Succeed to add cluster conf"
+                break
+            fi
+            log_error "${LINENO} Failed to add cluster conf"
+            sleep 5
+        done
+    fi
+fi
+
 config_maps=$(curl --cacert ${rootCAFile} -X GET -H "Authorization: Bearer $tokenFile" https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/dpa/configmaps/common-conf)
 is_exist=$(echo "${config_maps} " | python3 -c "import sys, json;print(json.load(sys.stdin)['data'].get('loglevel'))")
 if [ "${is_exist}" == "None" ];then
