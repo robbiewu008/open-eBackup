d213d1c0ebc70d81dc08a7e900ecb4d024e474ed
* <*>
2025-04-01 11:12:39 +0800

    Match-id-93674b5af82ba9c8309624a614327d7896b56ad9

--- build/package_open_source_agent.sh
+++ build/package_open_source_agent.sh
@@ -16,7 +16,8 @@ export WORKSPACE=${1:-"/usr1/"} # 工作目录
 export code_path=${2:-"${WORKSPACE}/REST_API"}  # 开源代码存放路径
 export binary_path=${3:-"${WORKSPACE}/open-source-obligation"} # 开源二进制文件存放路径
 export tag_image="release"
-export BUILD_TYPE=${4:-"release"}
+export BUILD_TYPE="release"
+export SOURCE_TYPE=$4
 export MS_IMAGE_TAG=${5:-"1.6.RC2.010"}
 export Version=${6:-"1.6.RC2"}
 export INTERNAL_VERSION=${7:-"1.6.0-RC2"}
@@ -39,8 +40,8 @@ echo AGENT_CODE_HOME=$AGENT_CODE_HOME
 # 外置插件包编译
 if [ $(uname -m) == "x86_64" ]; then
     echo "This is a x86_64 system."
-    sh "$code_path"/src/ProtectAgent/component/protectagent/protectagent/Agent/ci/script/build_opensource.sh x86 ${BUILD_TYPE}
+    sh "$code_path"/src/ProtectAgent/component/protectagent/protectagent/Agent/ci/script/build_opensource.sh x86 ${BUILD_TYPE} ${SOURCE_TYPE}
 elif [ $(uname -m) == "aarch64" ]; then
     echo "This is an aarch64 system."
-    sh "$code_path"/src/ProtectAgent/component/protectagent/protectagent/Agent/ci/script/build_opensource.sh arm ${BUILD_TYPE}
+    sh "$code_path"/src/ProtectAgent/component/protectagent/protectagent/Agent/ci/script/build_opensource.sh arm ${BUILD_TYPE} ${SOURCE_TYPE}
 fi
