d3d870bfbdc53434a74ed40a5260022d039b51e2
* <*>
2025-05-15 19:47:17 +0800

    Match-id-14f25fafba2ae5ee69d532a3ebe061d0bf25c9c3

--- src/ProtectManager/component/PM_GUI/src/service/console/src/app/business/protection/storage/storage-device-info/add-storage/add-storage.component.ts
+++ src/ProtectManager/component/PM_GUI/src/service/console/src/app/business/protection/storage/storage-device-info/add-storage/add-storage.component.ts
@@ -1,15 +1,15 @@
 /*
-* This file is a part of the open-eBackup project.
-* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
-* If a copy of the MPL was not distributed with this file, You can obtain one at
-* http://mozilla.org/MPL/2.0/.
-*
-* Copyright (c) [2024] Huawei Technologies Co.,Ltd.
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*/
+ * This file is a part of the open-eBackup project.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) [2024] Huawei Technologies Co.,Ltd.
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ */
 import {
   Component,
   Input,
@@ -95,7 +95,11 @@ export class AddStorageComponent implements OnInit {
     .filter(item => {
       if (
         includes(
-          [DataMap.Deploy_Type.e6000.value, DataMap.Deploy_Type.decouple.value],
+          [
+            DataMap.Deploy_Type.e6000.value,
+            DataMap.Deploy_Type.decouple.value,
+            DataMap.Deploy_Type.openServer.value
+          ],
           this.i18n.get('deploy_type')
         )
       ) {
@@ -113,7 +117,9 @@ export class AddStorageComponent implements OnInit {
       DataMap.Deploy_Type.x6000.value,
       DataMap.Deploy_Type.x9000.value,
       DataMap.Deploy_Type.e6000.value,
-      DataMap.Deploy_Type.decouple.value
+      DataMap.Deploy_Type.decouple.value,
+      DataMap.Deploy_Type.openOem.value,
+      DataMap.Deploy_Type.openServer.value
     ],
     this.i18n.get('deploy_type')
   );
